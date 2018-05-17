32e04b97fdf92482522ebdfdc233b03dfaef8817
naveenieus <naveenieus@gmail.com>
2018-05-17 17:59:05 +0530

    Analytics Link

--- js/analytics.js
+++ js/analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-119050127-1', 'auto');
+ga('create', 'UA-119410165-1', 'auto');
 ga('send', 'pageview');
